43a8945e63f7c8996da699dae0a9f9240084214f
George Pikramenos <george.pikramenos@hotmail.com>
2021-04-07 01:53:08 +0300

    add ips

--- player_auxilliary.py
+++ player_auxilliary.py
@@ -10,13 +10,21 @@ OUTPUT_END = "$ OUTPUT END"
 
 cmd_compile_player = ['python', 'compile.py', 'Programs/aa']
 
-coordinator = "http://0.0.0.0:12314/api/result"
+# coordinator = "http://0.0.0.0:12314/api/result"
+coordinator = "http://dl056.madgik.di.uoa.gr:12314/api/result"
+
+# ClientsRepo = {
+#     "0": "http://0.0.0.0:9000",
+#     "1": "http://0.0.0.0:9001",
+#     "2": "http://0.0.0.0:9002"
+# }
 
 ClientsRepo = {
-    "0": "http://0.0.0.0:9000",
-    "1": "http://0.0.0.0:9001",
-    "2": "http://0.0.0.0:9002"
+    "2": "http://dl058.madgik.di.uoa.gr:9002",
+    "1": "http://dl057.madgik.di.uoa.gr:9001",
+    "0": "http://dl056.madgik.di.uoa.gr:9000"
 }
+
 
 def handle_output(player_id, line_output, client_list):
     if player_id != 0:
